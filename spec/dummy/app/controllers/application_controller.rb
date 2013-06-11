24c2b6e662b46b08349cc1ff2647619ba6c1565e
Travis Luong <travis@travisluong.com>
2013-06-10 22:55:31 -0700

    add helper and info page for dummy app

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper StaticBlocks::Engine.helpers
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
+  get "info/index"
+
   mount StaticBlocks::Engine => "/static_blocks_admin"
 end
