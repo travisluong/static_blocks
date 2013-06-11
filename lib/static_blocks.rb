6000c914d7b2171f677871cf84ad6cd45c776440
Travis Luong <travis@travisluong.com>
2013-06-10 22:11:08 -0700

    bootstrap-wysihtml5-rails

--- lib/static_blocks.rb
+++ lib/static_blocks.rb
@@ -1,7 +1,4 @@
 require "static_blocks/engine"
-require "jquery-rails"
-require "sass-rails"
-require "bootstrap-sass"
 
 module StaticBlocks
 end
--- lib/static_blocks/engine.rb
+++ lib/static_blocks/engine.rb
@@ -1,3 +1,8 @@
+require "jquery-rails"
+require "sass-rails"
+require "bootstrap-sass"
+require "bootstrap-wysihtml5-rails"
+
 module StaticBlocks
   class Engine < ::Rails::Engine
     isolate_namespace StaticBlocks
