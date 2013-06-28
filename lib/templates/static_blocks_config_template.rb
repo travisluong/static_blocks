22ca2b64895ec09ece834fbb367196c9280bd7d5
Travis Luong <travis@travisluong.com>
2013-06-27 21:04:50 -0700

    update config template file

--- lib/templates/static_blocks_config_template.rb
+++ lib/templates/static_blocks_config_template.rb
@@ -3,4 +3,5 @@ StaticBlocks.config do |config|
   config.http_auth = false
   config.username = ENV['STATIC_BLOCKS_USERNAME']
   config.password = ENV['STATIC_BLOCKS_PASSWORD']
+  config.wysihtml5 = true
 end
